8c37857534033dbc415cf8a2fa3225e0a8ff483f
Sai-Pat <157099340+Sai-Pat@users.noreply.github.com>
2024-12-17 22:24:06 +0530

    Update Experiment_8.cpp
    updated!!!

--- Experiment_8.cpp
+++ Experiment_8.cpp
@@ -1,90 +1,111 @@
-#include<iostream>
-#include<graphics.h>
-#include<stdlib>
-#include<maths>
+//draw pattern
+#include <iostream>
+#include <stdlib.h>
+#include <graphics.h>
+#include<math.h>
+using namespace std;
+class Line
+{
+public:
+void dda(int x1,int y1,int x2,int y2)
+{
+   float x,y,dx,dy,length,xincr,yincr;
+   int i;
+   dx = abs(x2-x1);
+   dy = abs(y2-y1);
+   if(dx>dy)
+      {length = dx;}
+   else
+      {length = dy;}
 
-        void dda(int x1,int x2,int y1,int y2)
-        {
-            float x,y,dx,dy,step,xinc,yinc;
-            int i;
-            dx=abs(x2-x1);
-            dy=abs(y2-y1);
-            if(dx>dy){
-                step=dx;
-            }
-            else{
-                step=dy;
-            }
-            xinc=(x2-x1)/(float)step;
-            yinc=(y2-y1)/(float)step;
-            x=x1+0.5;
-            y=y1+0.5;
-            for(int i=0;i<=step;i++)
-            {
-                putpixel(x,y,12);
-                x=x+xinc;
-                y=y+yinc;
-            }
-            putpixel(x,y,12);
+   xincr= (x2-x1)/(float)length;
+   yincr= (y2-y1)/(float)length;
 
-        }
+   x = x1 + 0.5;
+   y = y1 + 0.5;
 
-        void bc(float x1, float y1,int r)
-        {
-            float Pk,x,y;
-            x=3-(2*r);
-            x=0;
-            y=r;
-            while(x<=y)
-            {
-                plot(x,y,x1,y1);
-                if (Pk<0){
-                    x=x+1;
-                    x=Pk+(4*x)+6;
-                }
-                else{
-                    x=x+1;
-                    y=y-1;
-                    x=Pk+(4*(x-y))+10;
-                }
-            }
-        }
-        void plot(int x,int y,int x1, int y1)
-        {
-            putpixel(x+x1,y+y1,7);
-            putpixel(y+x1,x+y1,7);
-            putpixel(-x+x1,y+y1,7);
-            putpixel(y+x1,-x+y1,7);
-            putpixel(x+x1,-y+y1,7);
-            putpixel(-y+x1,x+y1,7);
-            putpixel(-x+x1,-y+y1,7);
-            putpixel(-y+x1,-x+y1,7);
-            delay(500);
-        }
+   for(i=1;i<=length;i++)
+   {
+      putpixel(x,y,15);
+      x = x + xincr;
+      y = y + yincr;
+      }
+    putpixel(x,y,15);
+ } //end of DDA function
+}; //end of class Line
 
+class Circle
+{
+public:
+
+void drawcircle(float x1,float y1,int r)
+{
+    float x,y,d;
+    d = 3 - (2 * r);
+    x=0;
+    y=r;
+
+    while(x<=y)
+    {
+       plot(x,y,x1,y1);
+       if(d<0)
+	    {
+
+	  x=x+1;
+	  d = d + (4 * x) + 6;
+	    }
+       else
+	   {
+
+	  x=x+1;
+	  y=y-1;
+	  d = d + (4 * (x - y)) + 10;
+	   }
+}
+}
+void plot(int x,int y,int x1,int y1)
+{
+  putpixel(x+x1,y+y1,7);
+  putpixel(y+x1,x+y1,8);
+  putpixel(-x+x1,y+y1,9);
+  putpixel(y+x1,-x+y1,10);
+  putpixel(x+x1,-y+y1,11);
+  putpixel(-y+x1,x+y1,12);
+  putpixel(-x+x1,-y+y1,13);
+  putpixel(-y+x1,-x+y1,14);
+  delay(500);
+}
+};
 int main()
 {
-    int gd=DETECT, gm;
-    int x,y,r1,r2,x1,y1,x2,y2,x3,y3;
-    cout<<"Enter Center :";
-    cin>>x>>y;
-    cout<<"Enter Outer Radius : ";
-    cin>>r1;
-    cout<<"Enter Inner Radius : ";
-    cin>>r2;
-    initgraph(&gd,&gm,NULL);
-    bc(x,y,r1);
-    bc(x,y,r2);
-    int l;
-    l=sqrt((r1*r1)-(r2*r2));
-    x1=x;
-    y1=y-r1;
-    x2=x-l;
-    y2=y+r2;
-    x3=x+l;
-    y3=y+r2;
-    dda(x1,y1,x2,y2);
-    dda(x1,y1,x3,y3);
-    dda(x2,y2,x3,y3);
-    return 0;
+
+int gd=DETECT,gm;
+int x,y,r1,r2,x1,y1,x2,y2,x3,y3;
+cout<<"Enter centre coordinate (x,y)of circle";
+cin>>x>>y;
+cout<<"Enter radius of outer circle ";
+cin>>r1;
+cout<<"Enter radius of inner circle";
+cin>>r2;
+
+float length;
+length=sqrt((r1*r1)-(r2*r2));
+x1=x;
+y1=y-r1;
+x2=x-length;
+y2=y+r2;
+x3=x+length;
+y3=y+r2;
+Line obj;
+Circle c;
+initgraph(&gd,&gm,NULL);
+c.drawcircle(x,y,r1);
+c.drawcircle(x,y,r2);
+obj.dda(x1,y1,x2,y2);delay(500);
+obj.dda(x1,y1,x3,y3);delay(500);
+obj.dda(x2,y2,x3,y3);delay(5000);
+//getch();
+closegraph();
+system("clear");
+return 0;
 }
