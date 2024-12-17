5073e9d489dfa86e17c29dc4665c030646e8ee60
Sai-Pat <157099340+Sai-Pat@users.noreply.github.com>
2024-12-17 22:25:31 +0530

    Update Experiment_14.cpp
    updated!!!

--- Experiment_14.cpp
+++ Experiment_14.cpp
@@ -1,25 +1,46 @@
+//#include<dos.h>
 #include<iostream>
 #include<graphics.h>
 #include<math.h>
+//#include<conio.h>
 int main()
-{
-    int gd= DETECT,gm;
-    intitgraph(&gd,&gm,NULL);
-    float x=1,y,j=0.5,count=0.1,i;
-    setcolor(01);
-    for(int k =0;k<7;k++){
-        for(i=90;i<270;i+=10){
-            y=cos(i*3.14/180)/j;
-            x+=5;
-            circle(x,y*100+200,01);
-            line(0,215,800,215);
-            delay(50);
-            clrscr();
-        }
-        j+=count;
-        count+=0.1;
-    }
-    getch();
-    closegraph();
-    return 0;
-}
+ {
+
+   int gd=DETECT,gm;
+   initgraph(&gd,&gm,NULL);
+   float x=1,y=0.00000,j=0.5,count=.1;
+   float r=15;
+   setcolor(14);
+   line(0,215,640,215);
+   for(int k=0;k<=7;k++)
+   {
+
+    for(float i=90;i<270;i+=10)
+    {
+ y=cos(((i*3.14)/180))/j;
+
+ if(y>0)
+ y=-y;
+ x+=5;
+
+ setcolor(14);
+ //setfillstyle(HATCH_FILL,RED);
+ circle(x,y*100+200,r);
+ floodfill(x,y*100+200,14);
+
+       delay(100);
+
+ setcolor(0);
+ //setfillstyle(HATCH_FILL,RED);
+ circle(x,y*100+200,r);
+ floodfill(x,y*100+200,0);
+
+       }
+
+      j+=count;
+      count+=.1;
+
+     }
+   //getch();
+ }  
+
